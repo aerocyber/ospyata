acc73cf8fcf4fdf44507531e1f6bb7de0a8b814b
Aditya Ajay <86760749+aerocyber@users.noreply.github.com>
2021-08-18 17:19:13 +0530

    Update manageOsmation.py.

--- ospyata/manageOsmation.py
+++ ospyata/manageOsmation.py
@@ -7,7 +7,7 @@
    Manage osmations: add, delete, edit, view.
 """
 
-def addOsmation(db, name, url, cat=None):
+def addOsmation(db: list, name: str, url: str, cat=None):
     """Add osmation to the included osmations.
 
     Args:
@@ -32,6 +32,55 @@ def addOsmation(db, name, url, cat=None):
                 cat=cat
             ))
         dat["Categories"] = cat
-    # TODO: Add validation
     db.append(dat)
     return db
+
+def delOsmation(db: list, name: str):
+    """Delete an osmation with the name as given.
+
+    Args:
+        db (list): List of osmations.
+        name (str): Name of osmation.
+
+    Returns:
+        db: List of osmations with name removed.
+    """
+    for i in db:
+        if name == i["Name"]:
+            _ = db.pop(db.index(i))
+            break
+    return db
+
+def modName(name: str, db: list, newName: name):
+    """Change the name of osmation.
+
+    Args:
+        name (str): Old name of osmation.
+        db (list): List of osmations.
+        newName (str): New name of osmation.
+
+    """
+    for i in db:
+        if i["Name"] == name:
+            i["Name"] = newName
+            break
+
+
+def changeURL(name: str, newUrl: str, db: str):
+    """Change URL of osmation.
+
+    Args:
+        name (str): Name to which the new url is to be associated with.
+        newUrl (str): [description]
+        db (list): List of osmations.
+    """
+    for i in db:
+        if i["Name"] == name:
+            i["URL"] = newUrl
+            break
+
+def remByName(name: str, db: list):
+    for i in db:
+        if name in i:
+            index = db.index(i)
+            _ = i.pop(index)
